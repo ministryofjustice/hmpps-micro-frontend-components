5f0abcd93f72653b160551b70dabb1585cd9b2f1
Daniel Burnley <daniel.burnley@digital.justice.gov.uk>
2023-08-04 12:05:41 +0100

    Expose components as api responses (#11)
    * Expose components as api responses
    
    * Remove middleware

--- server/routes/componentRoutes.ts
+++ server/routes/componentRoutes.ts
@@ -2,34 +2,35 @@ import { Router } from 'express'
 import { Services } from '../services'
 import config from '../config'
 import asyncMiddleware from '../middleware/asyncMiddleware'
-import getUserPassport from '../middleware/getUserPassport'
-import setUpCurrentUser from '../middleware/setUpCurrentUser'
-import populateClientToken from '../middleware/populateClientToken'
 
 export default function componentRoutes(services: Services): Router {
   const router = Router()
 
-  router.use(getUserPassport(services))
-  router.use(setUpCurrentUser(services))
-  router.use(populateClientToken())
-
   router.get(
     '/header',
     asyncMiddleware(async (req, res, next) => {
-      const { clientToken } = res.locals
-
-      const caseLoads = await services.userService.getUserCaseLoads(clientToken)
-
-      return res.render('components/header', {
-        caseLoads,
-        activeCaseLoad: caseLoads.find(caseLoad => caseLoad.currentlyActive),
-        changeCaseLoadLink: `${config.apis.dpsHomePageUrl}/change-caseload`,
-      })
+      const userToken = req.headers['x-user-token'] as string
+      const caseLoads = await services.userService.getUserCaseLoads(userToken)
+      res.render(
+        'components/header',
+        {
+          caseLoads,
+          activeCaseLoad: caseLoads.find(caseLoad => caseLoad.currentlyActive),
+          changeCaseLoadLink: `${config.apis.dpsHomePageUrl}/change-caseload`,
+        },
+        (_, html) => {
+          res.header('Content-Type', 'application/json')
+          res.send(JSON.stringify({ html }))
+        },
+      )
     }),
   )
 
   router.get('/footer', (req, res, next) => {
-    return res.render('components/footer')
+    res.render('components/footer', {}, (_, html) => {
+      res.header('Content-Type', 'application/json')
+      res.send(JSON.stringify({ html }))
+    })
   })
 
   return router
