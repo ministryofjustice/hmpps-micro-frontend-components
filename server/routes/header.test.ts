c771adde4d28ff6cb93265447124bfa6a9d6cbc6
whitfield-mj <matthew.whitfield@digital.justice.gov.uk>
2023-08-08 15:54:08 +0100

    fix supertests (#14)

--- server/routes/header.test.ts
+++ server/routes/header.test.ts
@@ -1,64 +1,79 @@
-import type { Express } from 'express'
 import request from 'supertest'
 import * as cheerio from 'cheerio'
 import nock from 'nock'
-import { appWithAllRoutes } from './testutils/appSetup'
 import { services } from '../services'
 import config from '../config'
+import createApp from '../app'
 
-let app: Express
+let app: Express.Application
 let prisonApi: nock.Scope
+let tokenVerificationApi: nock.Scope
+let authApi: nock.Scope
 beforeEach(() => {
   prisonApi = nock(config.apis.prisonApi.url)
-  app = appWithAllRoutes({ services: services() })
+  tokenVerificationApi = nock(config.apis.tokenVerification.url)
+  authApi = nock(config.apis.hmppsAuth.url)
+
+  app = createApp(services())
 })
 
 afterEach(() => {
   jest.resetAllMocks()
 })
 
-// TODO fix tests
+describe('GET /header', () => {
+  describe('basic components', () => {
+    beforeEach(() => {
+      prisonApi.get('/api/users/me/caseLoads?allCaseloads=true').reply(200, [])
+      tokenVerificationApi.post('/token/verify').reply(200, { active: true, username: 'TEST_USER' })
+      authApi.get('/api/user/me').reply(200, { name: 'Test User', activeCaseLoadId: 'LEI' })
+    })
 
-describe.skip('GET /header', () => {
-  it('should render digital prison services title', () => {
-    prisonApi.get('/api/users/me/caseLoads?allCaseloads=true').reply(200, [])
-    return request(app)
-      .get('/header')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .expect(res => {
-        const $ = cheerio.load(res.text)
-        expect($('a[href="/"]').text()).toContain('Digital Prison Services')
-      })
-  })
+    it('should render digital prison services title', () => {
+      return request(app)
+        .get('/header')
+        .set('x-user-token', 'token')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(res => {
+          const $ = cheerio.load(JSON.parse(res.text).html)
+          expect($('a[href="/"]').text()).toContain('Digital Prison Services')
+        })
+    })
 
-  it('should render user management link', () => {
-    prisonApi.get('/api/users/me/caseLoads?allCaseloads=true').reply(200, [])
-    return request(app)
-      .get('/header')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .expect(res => {
-        const $ = cheerio.load(res.text)
-        const manageDetailsLink = $('a[href="/account-details"]')
-        expect(manageDetailsLink.text()).toContain('F. Last')
-        expect(manageDetailsLink.text()).toContain('Manage your details')
-      })
-  })
+    it('should render user management link', () => {
+      return request(app)
+        .get('/header')
+        .set('x-user-token', 'token')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(res => {
+          const $ = cheerio.load(JSON.parse(res.text).html)
+          const manageDetailsLink = $('a[href="/account-details"]')
+          expect(manageDetailsLink.text()).toContain('T. User')
+          expect(manageDetailsLink.text()).toContain('Manage your details')
+        })
+    })
 
-  it('should render sign out link', () => {
-    prisonApi.get('/api/users/me/caseLoads?allCaseloads=true').reply(200, [])
-    return request(app)
-      .get('/header')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .expect(res => {
-        const $ = cheerio.load(res.text)
-        expect($('a[href="/sign-out"]').text()).toEqual('Sign out')
-      })
+    it('should render sign out link', () => {
+      return request(app)
+        .get('/header')
+        .set('x-user-token', 'token')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(res => {
+          const $ = cheerio.load(JSON.parse(res.text).html)
+          expect($('a[href="/sign-out"]').text()).toEqual('Sign out')
+        })
+    })
   })
 
   describe('case load switcher', () => {
+    beforeEach(() => {
+      tokenVerificationApi.post('/token/verify').reply(200, { active: true, username: 'TEST_USER' })
+      authApi.get('/api/user/me').reply(200, { name: 'Test User', activeCaseLoadId: 'LEI' })
+    })
+
     it('should display case load link if user has multiple caseloads', () => {
       prisonApi.get('/api/users/me/caseLoads?allCaseloads=true').reply(200, [
         {
@@ -78,10 +93,11 @@ describe.skip('GET /header', () => {
       ])
       return request(app)
         .get('/header')
+        .set('x-user-token', 'token')
         .expect(200)
-        .expect('Content-Type', /html/)
+        .expect('Content-Type', /json/)
         .expect(res => {
-          const $ = cheerio.load(res.text)
+          const $ = cheerio.load(JSON.parse(res.text).html)
           expect($(`a[href="${config.apis.dpsHomePageUrl}/change-caseload"]`).text().trim()).toEqual('Leeds')
         })
     })
@@ -98,12 +114,24 @@ describe.skip('GET /header', () => {
       ])
       return request(app)
         .get('/header')
+        .set('x-user-token', 'token')
         .expect(200)
-        .expect('Content-Type', /html/)
+        .expect('Content-Type', /json/)
         .expect(res => {
-          const $ = cheerio.load(res.text)
+          const $ = cheerio.load(JSON.parse(res.text).html)
           expect($(`a[href="${config.apis.dpsHomePageUrl}/change-caseload"]`).length).toEqual(0)
         })
+    })
+  })
+
+  describe('auth', () => {
+    it('should send 401 if no token provided', () => {
+      return request(app).get('/header').expect(401)
+    })
+
+    it('should send 401 if token not valid', () => {
+      tokenVerificationApi.post('/token/verify').reply(200, { active: false })
+      return request(app).get('/header').expect(401)
     })
   })
 })
