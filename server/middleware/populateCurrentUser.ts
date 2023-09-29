1f8fee41f772cbb6aae2df44af859f2a4abaedbd
whitfield-mj <matthew.whitfield@digital.justice.gov.uk>
2023-09-29 09:41:00 +0100

    fix external users header not seeing name and manage details link (#63)

--- server/middleware/populateCurrentUser.ts
+++ server/middleware/populateCurrentUser.ts
@@ -10,14 +10,12 @@ export default function populateCurrentUser(userService: UserService): RequestHa
     }
 
     try {
-      if (res.locals.user.authSource === 'nomis') {
-        const token = res.locals.user.token ?? (req.headers['x-user-token'] as string)
-        const user = await userService.getUser(token, req.auth)
-        if (user) {
-          res.locals.user = { ...user, ...res.locals.user, token }
-        } else {
-          logger.info('No user available')
-        }
+      const token = res.locals.user.token ?? (req.headers['x-user-token'] as string)
+      const user = await userService.getUser(token, req.auth)
+      if (user) {
+        res.locals.user = { ...user, ...res.locals.user, token }
+      } else {
+        logger.info('No user available')
       }
       next()
     } catch (error) {
