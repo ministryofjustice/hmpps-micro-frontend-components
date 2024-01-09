d2192ad24ad0e5b3201a2c1bc1a44ef115103dcb
Matthew Whitfield <matthew.whitfield@digital.justice.gov.uk>
2024-01-09 10:22:13 +0000

    add parent app to run feature tests against locally

---
+++ integration_tests/parent_app/server.ts
@@ -0,0 +1,9 @@
+// Require app insights before anything else to allow for instrumentation of bunyan and express
+import 'applicationinsights'
+
+import { app } from './server/index'
+import logger from './logger'
+
+app.listen(app.get('port'), () => {
+  logger.info(`Server listening on port ${app.get('port')}`)
+})
