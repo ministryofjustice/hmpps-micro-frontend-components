a468b254659df58bd9ad3f3d6e9868a52a1c299e
Matthew Whitfield <matthew.whitfield@digital.justice.gov.uk>
2024-01-10 14:58:56 +0000

    update integration_tests structure to hold all config

--- integration_tests/parent_app/logger.ts
+++
@@ -1,9 +0,0 @@
-import bunyan from 'bunyan'
-import bunyanFormat from 'bunyan-format'
-import config from './server/config'
-
-const formatOut = bunyanFormat({ outputMode: 'short', color: !config.production })
-
-const logger = bunyan.createLogger({ name: 'HMPPS Typescript Template', stream: formatOut, level: 'debug' })
-
-export default logger
