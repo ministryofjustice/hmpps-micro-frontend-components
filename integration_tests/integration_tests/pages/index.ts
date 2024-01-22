fe320af0e3bee8794c939cc3ba26d5ded93bb52e
whitfield-mj <matthew.whitfield@digital.justice.gov.uk>
2024-01-22 15:03:47 +0000

    Cypress d (#154)
    * add parent app to run feature tests against locally
    
    * update cypress config to run int tests from parent app
    
    * update integration_tests structure to hold all config
    
    * add feature tests for header and footer

--- integration_tests/integration_tests/pages/index.ts
+++ integration_tests/integration_tests/pages/index.ts
@@ -5,13 +5,43 @@ export default class IndexPage extends Page {
     super('This site is under construction...')
   }
 
-  headerUserName = (): PageElement => cy.get('[data-qa=connect-dps-common-header-user-name]')
+  header = {
+    headerPhaseBanner: (): PageElement => cy.get('[data-qa=connect-dps-common-environment-tag]'),
 
-  headerPhaseBanner = (): PageElement => cy.get('[data-qa=connect-dps-common-environment-tag]')
+    caseloadSwitcher: (): PageElement => cy.get('[data-qa=connect-dps-caseload-switcher]'),
 
-  caseloadSwitcher = (): PageElement => cy.get('[data-qa=connect-dps-caseload-switcher]')
+    user: {
+      name: (): PageElement => cy.get('[data-qa=connect-dps-common-header-user-name]'),
+      toggle: (): PageElement => this.header.user.name().parent(),
+      menu: (): PageElement => cy.get('#connect-dps-common-header-user-menu'),
+      signOutLink: (): PageElement => cy.get('#connect-dps-common-header-user-menu').find('a[href="/sign-out"]'),
+      manageDetailsLink: (): PageElement =>
+        cy.get('#connect-dps-common-header-user-menu').find('a[href="http://localhost:9091/auth/account-details"]'),
+    },
 
-  servicesMenuToggle = (): PageElement => cy.get('[data-qa=connect-dps-common-service-menu-toggle]')
+    services: {
+      toggle: (): PageElement => cy.get('[data-qa=connect-dps-common-service-menu-toggle]'),
+      menu: (): PageElement => cy.get('#connect-dps-common-header-services-menu'),
+      list: (): PageElement => cy.get('#connect-dps-common-header-services-menu ul li'),
+    },
 
-  searchToggle = (): PageElement => cy.get('[data-qa=connect-dps-common-search-toggle]')
+    search: {
+      toggle: (): PageElement => cy.get('[data-qa=connect-dps-common-search-toggle]'),
+      menu: (): PageElement => cy.get('#connect-dps-common-header-search-menu'),
+      input: (): PageElement => cy.get('#connect-dps-common-header-prisoner-search'),
+      submit: (): PageElement => cy.get('.connect-dps-common-header__search-menu-submit-btn'),
+    },
+  }
+
+  footer = {
+    services: {
+      list: (): PageElement => cy.get('#connect-dps-common-footer-services ul li'),
+    },
+
+    surveyMonkeyLink: (): PageElement => cy.get('a[href="https://eu.surveymonkey.com/r/HJTL6XS"]'),
+    accessibilityLink: (): PageElement => cy.get('a[href="https://external/new-dps/accessibility-statement"]'),
+    termsAndConditionsLink: (): PageElement => cy.get('a[href="https://external/new-dps/terms-and-conditions"]'),
+    privacyPolicyLink: (): PageElement => cy.get('a[href="https://external/new-dps/privacy-policy"]'),
+    cookiesPolicyLink: (): PageElement => cy.get('a[href="https://external/new-dps/cookies-policy"]'),
+  }
 }
