ea8e034145ad43f9847f90d3df65851428f9f0c8
lynnette-orr <lynnette.orr@digital.justice.gov.uk>
2024-11-15 16:02:58 +0000

    Rr-1094: Update logic for Learning and work progress tile (#230)
    * Initial commit
    
    * Remove establishment check
    
    * Fix integration tests

--- server/services/utils/getServicesForUser.ts
+++ server/services/utils/getServicesForUser.ts
@@ -388,9 +388,7 @@ export default (
       description: 'View and manage learning and work history, support needs, goals and progress.',
       href: config.serviceUrls.learningAndWorkProgress.url,
       navEnabled: true,
-      enabled: () =>
-        userHasRoles([Role.EducationWorkPlanEditor, Role.EducationWorkPlanViewer], roles) &&
-        isActiveInEstablishment(activeCaseLoadId, ServiceName.LEARNING_AND_WORK_PROGRESS, activeServices, false),
+      enabled: () => true,
     },
     {
       id: 'prepare-someone-for-release',
