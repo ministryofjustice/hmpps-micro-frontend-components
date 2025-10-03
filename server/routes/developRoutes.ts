ac74ca758003049613879fb92c16ab9cadb3b5ad
ushkarev <ushkarev@users.noreply.github.com>
2025-10-03 13:02:53 +0100

    CDPS-1394: Express 5 compatibility (#381)
    * Remove `asyncMiddleware` as it’s no longer needed with express 5
    * Remove indirection when adding `get` request handlers to a router – it is now unnecessary since there is no special async middleware and the router’s true methods accept more flexible arguments

--- server/routes/developRoutes.ts
+++ server/routes/developRoutes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express'
 import { Services } from '../services'
-import asyncMiddleware from '../middleware/asyncMiddleware'
 import authorisationMiddleware from '../middleware/authorisationMiddleware'
 import { AVAILABLE_COMPONENTS } from '../@types/AvailableComponent'
 import auth from '../authentication/auth'
@@ -19,23 +18,15 @@ export default function developRoutes(services: Services): Router {
     res.render('pages/index', { components: AVAILABLE_COMPONENTS })
   })
 
-  router.get(
-    '/header',
-    populateCurrentUser(services.userService),
-    asyncMiddleware(async (_req, res) => {
-      const viewModel = await controller.getHeaderViewModel(res.locals.user)
-      return res.render('pages/componentPreview', viewModel)
-    }),
-  )
+  router.get('/header', populateCurrentUser(services.userService), async (_req, res) => {
+    const viewModel = await controller.getHeaderViewModel(res.locals.user)
+    return res.render('pages/componentPreview', viewModel)
+  })
 
-  router.get(
-    '/footer',
-    populateCurrentUser(services.userService),
-    asyncMiddleware(async (_req, res) => {
-      const viewModel = await controller.getFooterViewModel(res.locals.user)
-      return res.render('pages/componentPreview', viewModel)
-    }),
-  )
+  router.get('/footer', populateCurrentUser(services.userService), async (_req, res) => {
+    const viewModel = await controller.getFooterViewModel(res.locals.user)
+    return res.render('pages/componentPreview', viewModel)
+  })
 
   return router
 }
