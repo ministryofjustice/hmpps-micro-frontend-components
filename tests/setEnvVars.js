4f2522519006fbb4045f05278b7aba04f2d837aa
Igor <igor.ushkarev1@justice.gov.uk>
2025-11-13 16:40:30 +0000

    Add new var to test environments too for consistency

--- tests/setEnvVars.js
+++ tests/setEnvVars.js
@@ -1,3 +1,4 @@
+process.env.ENVIRONMENT = 'dev'
 process.env.ENVIRONMENT_NAME = 'DEV'
 process.env.ACTIVITIES_URL = 'https://activities-test.hmpps.service.justice.gov.uk'
 process.env.ALERTS_API_URL = 'https://alerts-api-dev.hmpps.service.justice.gov.uk'
