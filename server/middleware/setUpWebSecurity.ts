1998e31eac904aa0e45ee66ae0eb49966057b58f
Ewan-Donovan <ewan.donovan@digital.justice.gov.uk>
2026-02-02 14:45:36 +0000

    Lint

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -34,10 +34,7 @@ export default function setUpWebSecurity(): Router {
           // <link href="http://example.com/" rel="stylesheet" nonce="{{ cspNonce }}">
           // This ensures only scripts we trust are loaded, and not anything injected into the
           // page by an attacker.
-          connectSrc: [
-            "'self'",
-            ...azureDomains
-          ],
+          connectSrc: ["'self'", ...azureDomains],
           scriptSrc: ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`],
           styleSrc: [
             "'self'",
