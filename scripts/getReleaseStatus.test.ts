915da9f75dd004416b8d392f96a9372f3c2944b1
Igor <igor.ushkarev1@justice.gov.uk>
2026-06-15 16:39:44 +0100

    Sort service config mappings

--- scripts/getReleaseStatus.test.ts
+++ scripts/getReleaseStatus.test.ts
@@ -6,33 +6,35 @@ import yaml from 'yaml'
 
 import { endpoints, getData } from './getReleaseStatus'
 
-const residentialLocationUrl = 'https://locations-inside-prison-api-dev.hmpps.service.justice.gov.uk'
-const reportingUrl = 'https://digital-prison-reporting-mi-ui-dev.hmpps.service.justice.gov.uk'
-const alertsUrl = 'https://alerts-api-dev.hmpps.service.justice.gov.uk'
+// NB: keep service list sorted
 const activitiesUrl = 'https://activities-test.hmpps.service.justice.gov.uk'
 const adjudicationsUrl = 'https://manage-adjudications-api-dev.hmpps.service.justice.gov.uk'
-const learningAndWorkProgressUrl = 'https://learning-and-work-progress-dev.hmpps.service.justice.gov.uk'
-const whereaboutsApiUrl = 'https://whereabouts-api-dev.service.justice.gov.uk'
-const csipApiUrl = 'https://csip-api-dev.hmpps.service.justice.gov.uk'
+const alertsUrl = 'https://alerts-api-dev.hmpps.service.justice.gov.uk'
 const caseNotesApiUrl = 'https://dev.offender-case-notes.service.justice.gov.uk'
-const prepareSomeoneForReleaseUrl = 'https://resettlement-passport-ui-dev.hmpps.service.justice.gov.uk'
 const cemoUrl = 'https://hmpps-electronic-monitoring-create-an-order-dev.hmpps.service.justice.gov.uk'
+const csipApiUrl = 'https://csip-api-dev.hmpps.service.justice.gov.uk'
+const learningAndWorkProgressUrl = 'https://learning-and-work-progress-dev.hmpps.service.justice.gov.uk'
 const manageApplicationsUrl = 'https://managing-prisoner-apps-staff-dev.hmpps.service.justice.gov.uk'
 const officialVisitsApi = 'https://official-visits-api-dev.hmpps.service.justice.gov.uk'
+const prepareSomeoneForReleaseUrl = 'https://resettlement-passport-ui-dev.hmpps.service.justice.gov.uk'
+const reportingUrl = 'https://digital-prison-reporting-mi-ui-dev.hmpps.service.justice.gov.uk'
+const residentialLocationUrl = 'https://locations-inside-prison-api-dev.hmpps.service.justice.gov.uk'
+const whereaboutsApiUrl = 'https://whereabouts-api-dev.service.justice.gov.uk'
 const allUrls = [
-  residentialLocationUrl,
-  reportingUrl,
-  alertsUrl,
-  csipApiUrl,
   activitiesUrl,
   adjudicationsUrl,
-  learningAndWorkProgressUrl,
-  whereaboutsApiUrl,
+  alertsUrl,
   caseNotesApiUrl,
-  prepareSomeoneForReleaseUrl,
   cemoUrl,
+  csipApiUrl,
+  learningAndWorkProgressUrl,
   manageApplicationsUrl,
   officialVisitsApi,
+  prepareSomeoneForReleaseUrl,
+  reportingUrl,
+  residentialLocationUrl,
+  whereaboutsApiUrl,
+  // NB: keep service list sorted
 ]
 const allUrlsExcludingResidentialLocation = allUrls.filter(url => url !== residentialLocationUrl)
 
@@ -91,19 +93,19 @@ describe('Get release status script', () => {
     expect(mockRedisClientMock.set).toHaveBeenCalledWith(
       'applicationInfo',
       JSON.stringify([
-        { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'activities', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'alerts', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'csipApi', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'reporting', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'residentialLocations', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'learningAndWorkProgress', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'whereabouts', activeAgencies: ['agency1', 'agency2'] },
         { app: 'caseNotesApi', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
         { app: 'cemo', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'csipApi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'learningAndWorkProgress', activeAgencies: ['agency1', 'agency2'] },
         { app: 'manageApplications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'officialVisitsApi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'reporting', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'residentialLocations', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'whereabouts', activeAgencies: ['agency1', 'agency2'] },
       ]),
     )
   })
@@ -120,7 +122,7 @@ describe('Get release status script', () => {
     )
   })
 
-  it('should not fail if it cant find the data in response', async () => {
+  it('should not fail if it can’t find the data in response', async () => {
     setMockSuccess([residentialLocationUrl], { some: 'stuff' })
     setMockSuccess(allUrlsExcludingResidentialLocation)
 
@@ -128,18 +130,18 @@ describe('Get release status script', () => {
     expect(mockRedisClientMock.set).toHaveBeenCalledWith(
       'applicationInfo',
       JSON.stringify([
-        { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'activities', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'alerts', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'csipApi', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'reporting', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'learningAndWorkProgress', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'whereabouts', activeAgencies: ['agency1', 'agency2'] },
         { app: 'caseNotesApi', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
         { app: 'cemo', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'csipApi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'learningAndWorkProgress', activeAgencies: ['agency1', 'agency2'] },
         { app: 'manageApplications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'officialVisitsApi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'reporting', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'whereabouts', activeAgencies: ['agency1', 'agency2'] },
       ]),
     )
   })
@@ -153,17 +155,17 @@ describe('Get release status script', () => {
     expect(mockRedisClientMock.set).toHaveBeenCalledWith(
       'applicationInfo',
       JSON.stringify([
-        { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'activities', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'csipApi', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'reporting', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'residentialLocations', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'learningAndWorkProgress', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'caseNotesApi', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
         { app: 'cemo', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'csipApi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'learningAndWorkProgress', activeAgencies: ['agency1', 'agency2'] },
         { app: 'manageApplications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'officialVisitsApi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'reporting', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'residentialLocations', activeAgencies: ['agency1', 'agency2'] },
       ]),
     )
   })
@@ -182,18 +184,18 @@ describe('Get release status script', () => {
 
     it('should use the stored data for app if it exists and no new data', async () => {
       const storedData = [
-        { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'activities', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'alerts', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'caseNotesApi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'cemo', activeAgencies: ['agency1', 'agency2'] },
         { app: 'csipApi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'manageApplications', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'officialVisitsApi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
         { app: 'reporting', activeAgencies: ['agency1', 'agency2'] },
         { app: 'residentialLocations', activeAgencies: ['agency1', 'agency2'] },
         { app: 'whereabouts', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'caseNotesApi', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'cemo', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'manageApplications', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'officialVisitsApi', activeAgencies: ['agency1', 'agency2'] },
       ]
 
       setMockSuccess([residentialLocationUrl], { some: 'stuff', activeAgencies: ['agency1', 'agency2', 'agency3'] })
@@ -206,18 +208,18 @@ describe('Get release status script', () => {
       expect(mockRedisClientMock.set).toHaveBeenCalledWith(
         'applicationInfo',
         JSON.stringify([
-          { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
           { app: 'activities', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
           { app: 'alerts', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'caseNotesApi', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'cemo', activeAgencies: ['agency1', 'agency2'] },
           { app: 'csipApi', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'manageApplications', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'officialVisitsApi', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
           { app: 'reporting', activeAgencies: ['agency1', 'agency2'] },
           { app: 'residentialLocations', activeAgencies: ['agency1', 'agency2', 'agency3'] },
           { app: 'whereabouts', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'caseNotesApi', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'cemo', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'manageApplications', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'officialVisitsApi', activeAgencies: ['agency1', 'agency2'] },
         ]),
       )
     })
@@ -225,18 +227,18 @@ describe('Get release status script', () => {
     it('should use the stored data for app with the info check disabled', async () => {
       process.env.INFO_DISABLED_APPS = 'alerts,whereabouts'
       const storedData = [
-        { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'activities', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
         { app: 'alerts', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'caseNotesApi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'cemo', activeAgencies: ['agency1', 'agency2'] },
         { app: 'csipApi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'manageApplications', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'officialVisitsApi', activeAgencies: ['agency1', 'agency2'] },
+        { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
         { app: 'reporting', activeAgencies: ['agency1', 'agency2'] },
         { app: 'residentialLocations', activeAgencies: ['agency1', 'agency2'] },
         { app: 'whereabouts', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'caseNotesApi', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'cemo', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'manageApplications', activeAgencies: ['agency1', 'agency2'] },
-        { app: 'officialVisitsApi', activeAgencies: ['agency1', 'agency2'] },
       ]
 
       setMockSuccess([residentialLocationUrl], { some: 'stuff', activeAgencies: ['agency1', 'agency2', 'agency3'] })
@@ -249,18 +251,18 @@ describe('Get release status script', () => {
       expect(mockRedisClientMock.set).toHaveBeenCalledWith(
         'applicationInfo',
         JSON.stringify([
-          { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
           { app: 'activities', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
           { app: 'alerts', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'caseNotesApi', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'cemo', activeAgencies: ['agency1', 'agency2'] },
           { app: 'csipApi', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'manageApplications', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'officialVisitsApi', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
           { app: 'reporting', activeAgencies: ['agency1', 'agency2'] },
           { app: 'residentialLocations', activeAgencies: ['agency1', 'agency2', 'agency3'] },
           { app: 'whereabouts', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'caseNotesApi', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'cemo', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'manageApplications', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'officialVisitsApi', activeAgencies: ['agency1', 'agency2'] },
         ]),
       )
     })
@@ -279,18 +281,18 @@ describe('Get release status script', () => {
         'applicationInfo',
         JSON.stringify([
           { app: 'residentialLocations', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
           { app: 'activities', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'adjudications', activeAgencies: ['agency1', 'agency2'] },
           { app: 'alerts', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'csipApi', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'reporting', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'learningAndWorkProgress', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'whereabouts', activeAgencies: ['agency1', 'agency2'] },
           { app: 'caseNotesApi', activeAgencies: ['agency1', 'agency2'] },
-          { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
           { app: 'cemo', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'csipApi', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'learningAndWorkProgress', activeAgencies: ['agency1', 'agency2'] },
           { app: 'manageApplications', activeAgencies: ['agency1', 'agency2'] },
           { app: 'officialVisitsApi', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'prepareSomeoneForReleaseUi', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'reporting', activeAgencies: ['agency1', 'agency2'] },
+          { app: 'whereabouts', activeAgencies: ['agency1', 'agency2'] },
         ]),
       )
     })
