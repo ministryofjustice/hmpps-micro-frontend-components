980b6081a6b5bbd870ff2b9b6336d1546c5f6355
Nick Johnson <nicholas.johnson@justice.gov.uk>
2026-09-10 17:39:30 +0100

    Migrate to Cypress 16.0.0

--- cypress.config.ts
+++ cypress.config.ts
@@ -38,6 +38,5 @@ export default defineConfig({
     excludeSpecPattern: '**/!(*.cy).ts',
     specPattern: 'integration_tests/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'integration_tests/support/index.ts',
-    allowCypressEnv: false,
   },
 })
